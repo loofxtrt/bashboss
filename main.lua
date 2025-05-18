c93128bd7451afe7a954e8adf865f6167ce4422a
loofxtrt <loofxtrt@gmail.com>
2025-05-18 17:39:36 -0300

    better main.lua

--- main.lua
+++ main.lua
@@ -13,48 +13,36 @@ GODOT = DIRECTORIES.GODOT
 BASH_FILES = require("lua.paths.bash_files")
 ALIASES = BASH_FILES.ALIASES
 
-local function write_cd_aliases()
-    local aliases = {
+local function write_all_aliases()
+    local cd_aliases = {
         seagate = CD(SEAGATE),
         coding = CD(CODING),
         godot = CD(GODOT)
     }
 
-    INOUT.write_aliases(aliases)
-end
-
-local function write_source_aliases()
-    local aliases = {
+    local source_aliases = {
         actenv = SOURCE("env/bin/activate"),
         actvenv = SOURCE("venv/bin/activate"),
     }
 
-    INOUT.write_aliases(aliases)
-end
-
-local function write_software_aliases()
-    local aliases = {
+    local software_aliases = {
         hyfetch = "USER=luna hyfetch",
         nvconf = CD("~/.config/nvim/"),
         nvtree = "tree ~/.config/nvim/"
     }
 
-    INOUT.write_aliases(aliases)
-end
-
-local function write_run_aliases()
-    local aliases = {
+    local run_aliases = {
         confortavel = CD("/media/luan/seagate/workspace/coding/projects/bots/confortavel") .. " && " .. SOURCE("/media/luan/seagate/workspace/coding/projects/bots/confortavel/env/bin/activate && /media/luan/seagate/workspace/coding/projects/bots/confortavel/env/bin/python /media/luan/seagate/workspace/coding/projects/bots/confortavel/main.py")
     }
 
-    INOUT.write_aliases(aliases)
+    local all_aliases = { cd_aliases, source_aliases, software_aliases, run_aliases }
+    for _, alias_group in ipairs(all_aliases) do
+        INOUT.write_aliases(alias_group)
+    end
 end
 
 local function main()
-    write_cd_aliases()
-    write_source_aliases()
-    write_software_aliases()
-    write_run_aliases()
+    write_all_aliases()
 end
 
 main()
