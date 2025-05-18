072f70fe11ef253a1b567cb9e51a7df37a1cfa8b
loofxtrt <loofxtrt@gmail.com>
2025-05-18 14:52:58 -0300

    temp main.lua

--- bash/aliases.sh
+++
@@ -1,5 +0,0 @@
-alias coding='cd /media/luan/seagate/workspace/coding/projects'
-alias seagate='cd /media/luan/seagate'
-alias godot='cd /media/luan/seagate/workspace/godot/projects'
-alias actenv='source env/bin/activate'
-alias actvenv='source venv/bin/activate'
--- main.lua
+++ main.lua
@@ -32,5 +32,29 @@ local function write_source_aliases()
     INOUT.write_aliases(aliases)
 end
 
-write_cd_aliases()
-write_source_aliases()
+local function write_software_aliases()
+    local aliases = {
+        hyfetch = "USER=luna hyfetch",
+        nvconf = CD("~/.config/nvim/"),
+        nvtree = "tree ~/.config/nvim/"
+    }
+
+    INOUT.write_aliases(aliases)
+end
+
+local function write_run_aliases()
+    local aliases = {
+        confortavel = CD("/media/luan/seagate/workspace/coding/projects/bots/confortavel") .. " && " .. SOURCE("/media/luan/seagate/workspace/coding/projects/bots/confortavel/env/bin/activate && /media/luan/seagate/workspace/coding/projects/bots/confortavel/env/bin/python /media/luan/seagate/workspace/coding/projects/bots/confortavel/main.py")
+    }
+
+    INOUT.write_aliases(aliases)
+end
+
+local function main()
+    write_cd_aliases()
+    write_source_aliases()
+    write_software_aliases()
+    write_run_aliases()
+end
+
+main()
