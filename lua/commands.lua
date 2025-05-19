96cfcaf4f6d118f8d1b37cf1a4cc2cf07702a94e
loofxtrt <loofxtrt@gmail.com>
2025-05-18 22:36:07 -0300

    typo

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,6 +1,6 @@
 local M = {}
 
--- essas funções só escrevem a string do comando, não executam ele. é útil pra ESCREVER .sh, mas não rodar eles em si
+-- essas funções só escrevem a string do comando, não executam ele. é útil pra ESCREVER um arquivo .sh, mas não rodar eles em si
 function M.CD(path)
     return "cd " .. path
 end
